0b58d166b8f794742327db891600a27004f576fb
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-05-02 22:42:48 +0100

    made the out line of the page i like with most boxs ready for food ingrediants ,also set the fetch api up

--- main.js
+++ main.js
@@ -1,4 +1,44 @@
+const sliderImages = []
+
+function sliderMenu(){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 let foodToSearch = null;
+
+const YOUR_APP_ID = "4f8a14de"
+const YOUR_APP_KEY = "9b9c0fb4904a63e366b8342ca9279f97"
+
+const requestUrl = `https://api.edamam.com/search?q=kale&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`
+
 
 function handleRecipeClick() {
   fetchRecipe(foodToSearch);
@@ -9,6 +49,14 @@ function handleFoodChange() {
 }
 
 async function fetchRecipe(food) {
-  //--- write your code below ---
-  //--- write your code above ---
+  fetch(requestUrl)
+  .then((response)=>response.json())
+  .then((data)=>{
+    let randint =Math.floor(Math.random()*data.hits.length)
+    return console.log( data.hits[randint].recipe)
+ 
+  })
+
 }
+
+fetchRecipe()
