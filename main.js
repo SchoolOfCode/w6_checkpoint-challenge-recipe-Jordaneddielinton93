630bac3275145e4a948ed3afff999d7ab8b151b8
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-05-06 13:58:14 +0100

    found a bug that searched twice when searchfood was clicked

--- main.js
+++ main.js
@@ -48,50 +48,67 @@ slideLeftButton.addEventListener("click",()=>{
   
 })
 
-// above is just for the slider menu 
+// above is just for the slider menu ----
+
+// Goal to build a timer that counts down from 5mins
+// 0. start with a function
+// 1. build timer outline 00.02.00
+// 2. use set interval 
+// 3. minues each number
+// 4
+let getMiddleNumber = document.getElementById("middle-Counter")
+let getRightNumber = document.getElementById("right-Counter")
+let MiddleCountDown = 4
+let RightCountDown = 60
+
+let CountDownTimer = setInterval(()=>{
+  getMiddleNumber.innerText = `0${MiddleCountDown}.`
+  RightCountDown = Math.floor(RightCountDown-1)
+  getRightNumber.innerText = RightCountDown
+  if(MiddleCountDown == 0 && RightCountDown == 0){
+    clearInterval(CountDownTimer)
+  }
+  if(RightCountDown == 0){
+    RightCountDown = 59
+    MiddleCountDown-=1
+  }
+  
+},1000)
 
 
 
-
-
+// below is the api to search for foods
 let foodToSearch = null;
 const YOUR_APP_ID = "4f8a14de"
 const YOUR_APP_KEY = "9b9c0fb4904a63e366b8342ca9279f97"
-
-// const requestUrl = `https://api.edamam.com/search?q=kale&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`
-
-
-
+// 1 when you press enter get search value (e:g words typed) and get api
 RecipeEnterKey = document.getElementById("food-input")
 RecipeEnterKey.addEventListener("keydown",(event)=>{
   if(event.key == "Enter"){
     console.log("Enter key pressed")
-    return handleRecipeClick()
+    return handleFoodChange()
   }
 })
+// 2 when clicked get search value 
 RecipeClick = document.getElementById("recipe-button")
-RecipeClick.addEventListener("click",handleRecipeClick)
-function handleRecipeClick() {
-  handleFoodChange()
-  foodFoundObject = fetchRecipe(foodToSearch);
-}
+RecipeClick.addEventListener("click",handleFoodChange)
 
-
-
+// 3. check if there is a value typed and call api
 function handleFoodChange() {
   foodToSearch = document.getElementById("food-input").value;
   if(foodToSearch.length <= 1){
     return alert("type a word before pressing enter")
   }else{
-    return foodToSearch
+    return fetchRecipe(foodToSearch)
   }
 }
+// 4 get recipe searched for and fill in the info on screen (e:g images, ingrediants)
 async function fetchRecipe(food) {
   let response = await fetch(`https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`)
-  responseData = await response.json()
-  let randNum = Math.floor(Math.random()*responseData.hits.length)
-  console.log(responseData)
-  let randRecipe = responseData.hits[randNum].recipe
+  let data = await response.json()
+  let randNum = Math.floor(Math.random()*data.hits.length)
+  console.log(data)
+  let randRecipe = data.hits[randNum].recipe
   getImagesURL(randRecipe.image)
   getfoodLabel(randRecipe.label)
   getDishType(randRecipe.dishType)
