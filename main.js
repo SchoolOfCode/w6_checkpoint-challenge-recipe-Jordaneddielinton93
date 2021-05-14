ef3846cbd14dab4e76f7b8d840dcc9fbed26857f
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-05-14 17:11:04 +0100

    turned all api's back on

--- main.js
+++ main.js
@@ -157,7 +157,7 @@ let CountDownTimer = setInterval(()=>{
   if(MinuteCountDown == 0 && SecoundsCountDown == 0){
     MinuteCountDown = 1
     TurnOnRandImageByCounter = true
-    // getImagesLabel()<<<<<<<<<<<<turn off
+    getImagesLabel() //<<<<<<<<<<<<turn off
   }
   if(SecoundsCountDown == 0){
     SecoundsCountDown = 59
@@ -197,7 +197,7 @@ function handleFoodChange() {
 
 // 4 get recipe searched for and fill in the info on screen (e:g images, ingrediants)
 async function fetchRecipe(food) {
-  let response = await fetch(`https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`)//.catch(imageReloader)//<<<<<<<<<<<<turn off api
+  let response = await fetch(`https://api.edamam.com/search?q=${food}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`).catch(imageReloader)//<<<<<<<<<<<<turn off api
   let data = await response.json()
   let randNum = Math.floor(Math.random()*data.hits.length)
   console.log(data.hits[2].recipe)
@@ -255,7 +255,7 @@ function imageReloader(){
   console.log("YOU HAVE REQUESTED TO MANY TIMES THE SERVER LIMIT IS 10 REQUESTS PER MINUTE IT WILL NOW RELOAD THE REQUEST IN 1 MINUTE"),reloader = true
   if(reloader == true)
     setTimeout(()=>{
-      // getImagesLabel()  //<<<<<<<<<<<<<<<<<<<<<turn off api
+      getImagesLabel()  //<<<<<<<<<<<<<<<<<<<<<turn off api
       console.log("reloading now :)")
       reloader = false
     },68000)
@@ -299,7 +299,7 @@ async function getImagesLabel(){
     getInnerPTag(3,burgerResults,number)
 
 }
-// getImagesLabel()//<<<<<<<<<<<<<<<<<<<<<turn off api
+getImagesLabel()//<<<<<<<<<<<<<<<<<<<<<turn off api
 
 
 const APP_KEY = "rI1jjxNbYiJn2GINSQNujhjjLaPQePNMOb-l2s6Nlps";
@@ -390,6 +390,7 @@ function movingLeaf(){
     leaf1Left++
     leaf1Top--
     leaf1Top+=3
+    
     if(leafTop > 1000){
       let randNumb = Math.floor(Math.random()*600)
       let randNumb1 = Math.floor(Math.random()*-200)
